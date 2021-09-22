547b3d523e430cd65502b6cd11cf9a2a82b92fee
Ruben Peters1 <ruben.peters1@jdplc.com>
2021-09-22 21:49:22 +0100

    clean up search function

--- src/component/UI/Search.js
+++ src/component/UI/Search.js
@@ -8,20 +8,7 @@ import {colours} from '../../assets/coloursData';
 
 class Search extends Component {
     state = {
-        name: "",
-        coloursData: [colours],
-        imageUrl: "",
         pokemon: "",
-        types: [],
-        abilities: [],
-        stats: {
-            hp: "",
-            attack: "",
-            defense: "",
-            specialAttack: "",
-            specialDefense: "",
-            speed: "",
-        },
         display: false,
     }
     
@@ -32,38 +19,8 @@ class Search extends Component {
             try {
             const url = `https://pokeapi.co/api/v2/pokemon/${this.state.pokemon}`
             const res = await axios.get(url)
-            const name = res.data.name;
-            const imageUrl = res.data.sprites.front_default;
-            const types = res.data.types.map(type => type.type.name)
-            const abilities = res.data.abilities.map(ability => {
-                return ability.ability.name.toLowerCase()
-                .split('-')
-                .map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')
-            }).join(', ');
-            let {hp, attack, defense, specialAttack, specialDefense, speed } = '';
-            res.data.stats.map(stat => {
-                switch(stat.stat.name) {
-                    case 'hp':
-                        hp = stat['base_stat']
-                        break;
-                    case 'attack':
-                        attack = stat['base_stat']
-                        break;
-                    case 'defense':
-                        defense = stat['base_stat']
-                        break;
-                    case 'special-attack':
-                        specialAttack = stat['base_stat']
-                        break;
-                    case 'special-defense':
-                        specialDefense = stat['base_stat']
-                        break;
-                    case 'speed':
-                        speed = stat['base_stat']
-                        break;
-                }
-            });
-            this.setState({name, imageUrl, types, abilities, hp, attack, defense, specialAttack, specialDefense, speed})
+            
+            
             this.setState({display: true})
             } catch (error) {
             console.log(error)
